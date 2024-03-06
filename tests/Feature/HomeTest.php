eaf0b8382eef1decfbe2993522a91ce19cf4b9c0
Gustavo Valmaña <gustavo.shotokai@gmail.com>
2024-03-05 22:05:04 -0500

    first commit

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    use RefreshDatabase;
+    public function testEmpty(): void
+    {
+        $this->get('/')
+            ->assertStatus(200)
+            ->assertSee('No hay etiquetas');
+    }
+
+    public function testWithTags(): void
+    {
+        $tag = Tag::factory()->create();
+        $this->get('/')
+            ->assertStatus(200)
+            ->assertSee($tag->name)
+            ->assertDontSee('No hay etiquetas');
+    }
+}
